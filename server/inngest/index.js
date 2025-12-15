08fbeaee3cbd9e532a0ec5fd166e5a8fbbaea625
Karan Kumbhare <karankumbhare90@gmail.com>
2025-12-15 21:41:25 +0530

    Half Done

---
+++ server/inngest/index.js
@@ -0,0 +1,60 @@
+import { Inngest } from 'inngest'
+import prisma from '../config/prisma';
+
+export const inngest = new Inngest({ id: "TaskSynthex" })
+
+// Inngest function to create user
+const syncUserCreation = inngest.createFunction(
+    { id: `sync-user-from-clerk` },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.create({
+            data: {
+                id: data?.id,
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url
+            }
+        })
+    }
+)
+
+// Inngest function to update user
+const syncUserUpdation = inngest.createFunction(
+    { id: `update-user-from-clerk` },
+    { event: "clerk/user.updated" },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.create({
+            where: {
+                id: data?.id
+            },
+            data: {
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url
+            }
+        })
+    }
+)
+
+// Inngest function to delete user from DB
+const syncUserDeletion = inngest.createFunction(
+    { id: `delete-user-from-clerk` },
+    { event: "clerk/user.deleted" },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.delete({
+            where: {
+                id: data?.id,
+            }
+        })
+    }
+)
+
+export const functions = [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+];
---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import { clerkMiddleware } from '@clerk/express'
+import 'dotenv/config'
+import { functions, inngest } from './inngest/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => {
+    return res.json({
+        success: true, status: 201, message: `Hello Developer !!`
+    })
+})
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT : ${PORT}`);
+})
